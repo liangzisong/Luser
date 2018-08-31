8af6eac003ffd8c0f2991dd89c44f1cc93e7b659
liang <18462146261@163.com>
2018-08-31 18:02:16 +0800

    添加了mybatis与druid，未完成

--- src/main/java/com/Luser/LuserApplication.java
+++ src/main/java/com/Luser/LuserApplication.java
@@ -2,8 +2,12 @@ package com.Luser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ServletComponentScan
+@ImportResource(locations = { "classpath:druid-bean.xml" })
 public class LuserApplication {
 
 	public static void main(String[] args) {
